86a70bf0890f790048f3dfa7653bfccd5a1cb861
Yargo Bonetti DPHYS <yargo@phys.ethz.ch>
2018-07-05 10:26:43 +0200

    add minimum time value, correct handling of 0 time, add argument reporting

--- convlog.sh
+++ convlog.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # shell script for converting logfiles created with webforms
-info='convlog.sh // 2018-05-01 Y.Bonetti // http://github.com/hb9kns/webforms'
+info='convlog.sh // 2018-07-05 Y.Bonetti // http://github.com/hb9kns/webforms'
 
 # minutes/day
 dm=$(( 24*60 ))
+# general minimum value
+dmin=5
 
 if test "$1" = ""
 then cat <<EOT
@@ -12,11 +14,12 @@ $info
 
 convert logfile (from STDIN) generated with webforms.cgi
 
-usage: $0 [-s] [-m MM|-n] [-x XX] <c>
+usage: $0 [-s] [-m MM|-n] [-x XX] [-i II] <c>
  convert STDIN using columns <c> and <c+1> with absolute minute stamps,
  adding (<c+1>)-(<c>) in column <c+2>, using maximum value MM (-m)
  or $dm-(<c>) (-n, midnight logout) if <c+1> is unreadable as minutes,
  using maximum value XX (-x, or $dm if not given),
+ general minimum value II (-i, or $dmin if not given),
  keeping all other columns and printing to STDOUT,
  not adding column <c+2> in case of option -s
  Notes:
@@ -29,11 +32,14 @@ exit 9
 fi
 
 # preset all options to invalid or maximum
+begcol=-1
 daymin=-1
 maxmin=-1
 maxmax=$dm
+minmin=$dmin
 same=no
 
+args="$*"
 while test "$1" != ""
 do
  case $1 in
@@ -42,20 +48,26 @@ do
  -m) maxmin=${2:-0} ; daymin=-1 ; shift ;;
 # absolute maximum in minutes
  -x) maxmax=${2:-$dm} ; shift ;;
+ -i) minmin=${2:-$dmin} ; shift ;;
  -s) same=yes ; shift ;;
  *) begcol=$1 ;;
  esac
  shift
 done
 
-cat <<EOI >&2
-: $0
-: logical timestamp start column: $begcol
-: daymin: $daymin
-: maxmin: $maxmin
-: maxmax: $maxmax
-: processing...
+cat <<EOI
+### $0 $args
+### logical timestamp start column: $begcol
+### daymin: $daymin
+### minmin: $minmin
+### maxmin: $maxmin
+### maxmax: $maxmax
 EOI
+
+if test $begcol -le 0
+then echo "illegal column $begcol, aborting!" >&2
+ exit 1
+fi
 
 # physical columns up to, begin, end, from/after time stamp
 tocol=$begcol
@@ -85,7 +97,6 @@ EOT
   ;;
 # if line begins with +/-
  -*|+*)
-#echo ":: $front :: '$begval' -- '$endval' :: $rear //" >&2
 # get minute values: remove everything up to '=' from timestamp
 # and set to 0, if no '=' found
   begmin=${begval##*=}
@@ -109,12 +120,15 @@ EOT
   else effmin=-1
   fi
 # in case of illegal minutes (negative or more than a day)
-  if test $effmin -le 0 -o $effmin -gt $dm
+  if test $effmin -lt 0 -o $effmin -gt $dm
   then effmin=$limmin
   fi
 # limit value
   if test $effmin -gt $maxmax
   then effmin=$maxmax
+  fi
+  if test $effmin -lt $minmin
+  then effmin=$minmin
   fi
 # output line with additional column (note: TABs!) unless option -s
   if test $same = no
